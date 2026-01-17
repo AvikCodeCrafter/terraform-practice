65459bb960d4167e4a9f71314487dca6cda56d9d
Avik Kumar Banerjee <36740829+AvikCodeCrafter@users.noreply.github.com>
2026-01-17 19:36:34 +0530

    Update terraform.tf

--- terraform-aws-vpc/terraform.tf
+++ terraform-aws-vpc/terraform.tf
@@ -7,3 +7,11 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tf-state-bucket-3015986454"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
