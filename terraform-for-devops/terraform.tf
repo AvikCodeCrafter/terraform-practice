d9f05ca0a86434d34f993c87c90ad62b9326b38b
Avik Kumar Banerjee <36740829+AvikCodeCrafter@users.noreply.github.com>
2026-01-17 19:35:31 +0530

    Update terraform.tf

--- terraform-for-devops/terraform.tf
+++ terraform-for-devops/terraform.tf
@@ -6,3 +6,12 @@ terraform {
     use_lockfile = true
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "tf-state-bucket-3015986454"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
